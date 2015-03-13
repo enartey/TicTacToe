6305332b1d372f5f9ee17f497e832c0f5882445c
Edwin Nartey <narteye15@hanover.edu>
2015-03-13 11:50:57 -0400

    main.js setup. Issue #2

--- app/main.js
+++ app/main.js
@@ -1,1 +1,19 @@
+/*
+	Main file that loads things. 
+	Called from require.js
+*/
+
+require.config({
+   // baseUrl is automatically set to the directory where this main.js file is.
+   // Or we can set it in this config:
+   baseUrl: "app",
+   // This way one can call on jquery and libs directly
+   paths: {
+      "lib": "../lib",
+      "jquery": "../lib/jquery"
+      // or the following to load remotely:
+      // "jquery": "https://code.jquery.com/jquery-2.1.3.min"
+   },
+   waitSeconds: 15
+});
 
