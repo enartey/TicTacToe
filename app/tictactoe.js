a59995ebadb58a20663db0e46ad61c619c44fb0f
Edwin Nartey <narteye15@hanover.edu>
2015-03-19 11:57:45 -0400

    initialze method written #3

---
+++ app/tictactoe.js
@@ -0,0 +1,34 @@
+/*
+	File that creates the array used by the game to 
+	determine locate and notify the user when there is 
+	a winner
+*/
+
+var index, boardSize, board, proto;
+
+
+/* creates two dimensional game board of any size using a nested array */
+function makeBoard (){
+	var index1, index2, arr;
+	board = Object.create(proto);
+	return board;
+}
+
+
+proto = {
+	
+	initialize: function(boardSize){
+		var arr = [];
+		while (index1 < boardSize){
+			index2 = 0;
+			arr[index1] = [];
+			while (index2 < boardSize){
+				arr[index1][index2] = null;
+				index2 += 1;
+			}
+			index1 += 1;
+		}
+		return arr;
+	}
+
+};
