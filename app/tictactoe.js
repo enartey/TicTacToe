b31a847f4aa6fb45ddfba0ce15096c4813543a32
Edwin Nartey <narteye15@hanover.edu>
2015-03-22 23:28:57 -0400

    fully linted with joy. #11

--- app/tictactoe.js
+++ app/tictactoe.js
@@ -1,29 +1,27 @@
 /*
-	File that creates the array used by the game to 
-	determine locate and notify the user when there is 
+	File that creates the array used by the game to
+	determine locate and notify the user when there is
 	a winner
 */
 
-var index, boardSize, board, proto, i, j, p;
+var index, board, proto;
 
 
 /* creates two dimensional game board of any size using a nested array */
-function makeBoard (){
-	var index1, index2, arr;
+function makeBoard(){
 	board = Object.create(proto);
 	return board;
 }
 
 
 proto = {
-	
 	initialize: function(boardSize){
-		var arr = [];
+		var arr = [], index1, index2;
 		while (index1 < boardSize){
 			index2 = 0;
-			arr[index1] = [];
+			arr[ index1 ] = [];
 			while (index2 < boardSize){
-				arr[index1][index2] = null;
+				arr[ index1 ][ index2 ] = null;
 				index2 += 1;
 			}
 			index1 += 1;
@@ -32,58 +30,58 @@ proto = {
 	},
 
 	checkDL: function(i, j, p){
-		if(this[i-1][j-1] === p){
+		if (this[ i - 1 ][ j - 1 ] === p){
 			return true;
-		};
-		return false
+		}
+		return false;
 	},
 
 	checkDR: function(i, j, p){
-		if(this[i-1][j+1] === p){
+		if (this[ i - 1 ][ j + 1 ] === p){
 			return true;
-		};
+		}
 		return false;
 	},
 
 	checkUR: function(i, j, p){
-		if(this[i+1][j+1] === p){
+		if (this[ i + 1 ][ j + 1 ] === p){
 			return true;
-		};
+		}
 		return false;
 	},
 
 	checkUL: function(i, j, p){
-		if(this[i+1][j-1] === p){
+		if (this[ i + 1 ][ j - 1 ] === p){
 			return true;
-		};
+		}
 		return false;
 	},
 
 	checkL: function(i, j, p){
-		if(this[i-1][j] === p){
+		if (this[ i - 1 ][ j ] === p){
 			return true;
-		};
+		}
 		return false;
 	},
 
 	checkR: function(i, j, p){
-		if(this[i+1][j] === p){
+		if (this[ i + 1 ][ j ] === p){
 			return true;
-		};
+		}
 		return false;
 	},
 
 	checkD: function(i, j, p){
-		if(this[i][j-1] === p){
+		if (this[ i ][ j - 1 ] === p){
 			return true;
-		};
+		}
 		return false;
 	},
 
 	checkU: function(i, j, p){
-		if(this[i][j+1] === p){
+		if (this[ i ][ j + 1 ] === p){
 			return true;
-		};
+		}
 		return false;
 	},
 
