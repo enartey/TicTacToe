5199cd61d2489f924299aa40800fa1cd075834ff
Edwin Nartey <narteye15@hanover.edu>
2015-04-09 13:43:58 -0400

    further template changes made

--- app/controller.js
+++ app/controller.js
@@ -1,7 +1,22 @@
-define([],
-	var Controller;
+define(["jquery"],
+
+	function($){
+		var Controller;
+	}
+
+	function Controller(domElement, model){
+		this.el = $(domElement);
+		this.model = model;
+
+		this.render();
+		this.model.on("change", this.render, this);
+
+	}
 	
 	Controller.prototype = {
+		render: function(){
+			// TODO
+		}
 
 	return Controller;
 });
