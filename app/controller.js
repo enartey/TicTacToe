cde93d7e6423c37a30bd7fff4bc4925ebfb5e8ff
Edwin Nartey <narteye15@hanover.edu>
2015-04-14 20:28:21 -0400

    continued controller implementation

--- app/controller.js
+++ app/controller.js
@@ -11,13 +11,21 @@ define(["jquery"],
 		this.render();
 		this.model.on("change", this.render, this);
 		this.model.on("error", this.render, this);
-		this.el.on("change", )
+
+		// If we end up using select and input in our work this will catch changes from the webpage 
+		//this.el.on("change", "input", this.respondToChange.bind(this));
+		//this.el.on("change", "select", this.respondToChange.bind(this));
 	}
 	
 	Controller.prototype = {
 		render: function(){
-			// TODO
+			// set the HTML of element to equal result of applying template
+			this.el.html(template(this.model))
+		},
+		respondToChange: function(ev){
+			var el = $(ev.currentTarget);
+			this.model.set(el.data("key"), el.val());
 		}
-	}
+	};
 	return Controller;
 });
