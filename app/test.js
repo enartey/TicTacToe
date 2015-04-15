8084d1714b944ec18e1f6f6748ed3613ba74b702
Dakota <dakotaraymccoy@gmail.com>
2015-04-15 11:04:03 -0400

    test.js code added

---
+++ app/test.js
@@ -0,0 +1,32 @@
+/*
+	Main file that loads things. 
+	Called from require.js
+*/
+
+require.config({
+   // baseUrl is automatically set to the directory where this main.js file is.
+   // Or we can set it in this config:
+   baseUrl: "app",
+   // This way one can call on jquery and libs directly
+   paths: {
+      "lib": "../lib",
+      "jquery": "../lib/jquery"
+      // or the following to load remotely:
+      // "jquery": "https://code.jquery.com/jquery-2.1.3.min"
+   },
+   //waitSeconds: 15
+});
+
+require(["jquery","model","controller"],
+   function($, Model, Controller){
+      var model, controller;
+      $(function(){
+         console.log("page loaded");
+
+         model = new Model();
+         console.log("model: ", model);
+
+         controller = new Controller("main", model);
+      });
+   });
+
