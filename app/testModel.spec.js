94c29e01be3d329c164b30e4b9ffb9026a1fbb18
Dakota <dakotaraymccoy@gmail.com>
2015-04-15 14:40:02 -0400

    test work

--- app/testModel.spec.js
+++ app/testModel.spec.js
@@ -2,7 +2,8 @@ var board, expect, ticTacToe, chai,
 	a1 = [[0,0,0],[0,0,0],[0,0,0]],
 	a2 = [[1,1,1],[1,1,1],[1,1,1]],
 	a3 = [[0,0,0],[1,1,1],[0,1,0]],
-	a3 = [[1,1,1],[0,0,0],[1,0,1]];
+	a3 = [[1,1,1],[0,0,0],[1,0,1]],
+	nullArr = [[null, null, null],[null, null, null],[null, null, null]];
 
 //expect = require("./chai.js").expect;
 //ticTacToe = require("./model.js");
@@ -18,7 +19,6 @@ describe("constructor", function(){
 	});
 
 	it.skip("creates null array", function(){
-		var nullArr = [[null, null, null],[null, null, null],[null, null, null]];
 		expect(board.arr).to.deep.equal(nullArr);
 	});
 
@@ -29,7 +29,7 @@ describe("constructor", function(){
 	});
 });
 
-describe("methods", function(){
+describe("Board Methods", function(){
 	//i controls left/right axis
 	//j controls up/down
 	//(0,0) is bottom left spot on board
@@ -53,14 +53,52 @@ describe("methods", function(){
 		expect(board.isOutOfBounds(2, 1)).to.equal(false);
 		expect(board.isOutOfBounds(1, 2)).to.equal(false);
 	});
+});
 
-	it("check DL", function(){
-		board.arr = a1;
-		board.turn = 0;
-		//out of bounds examples first
-		expect(board.checkDL(0,0)).to.equal(false);
-		expect(board.checkDL(0,1)).to.equal(false);
-		expect(board.checkDL(0,0)).to.equal(false);
-		expect(board.checkDL(0,0)).to.equal(false);
+describe("Individual Check Functions", function(){
+	beforeEach(function(){
+		board = ticTacToe.new();
+	});
+	describe
+	describe("Null Board", function(){
+		it("Null Board", function(){
+			board.arr = nullArr;
+		});
+		it("Full O Board", function(){
+			//use a board full of O's [a1] and set to O's turn
+			board.arr = a1;
+			board.turn = 0;
+			//out of bounds examples first
+			expect(board.checkDL(0, 0)).to.equal(false);
+			expect(board.checkDL(0, 1)).to.equal(false);
+			expect(board.checkDL(0, 2)).to.equal(false);
+			expect(board.checkDL(1, 0)).to.equal(false);
+			expect(board.checkDL(2, 0)).to.equal(false);
+			//now run in bounds examples
+			expect(board.checkDL(1, 1)).to.equal(true);
+			expect(board.checkDL(1, 2)).to.equal(true);
+			expect(board.checkDL(2, 1)).to.equal(true);
+			expect(board.checkDL(2, 2)).to.equal(true);
+			//switch to board full of X's and make it X's turn
+			board.arr = a2;
+			board.turn = 1;
+			//out of bounds examples first
+			expect(board.checkDL(0, 0)).to.equal(false);
+			expect(board.checkDL(0, 1)).to.equal(false);
+			expect(board.checkDL(0, 2)).to.equal(false);
+			expect(board.checkDL(1, 0)).to.equal(false);
+			expect(board.checkDL(2, 0)).to.equal(false);
+			//now run in bounds examples
+			expect(board.checkDL(1, 1)).to.equal(true);
+			expect(board.checkDL(1, 2)).to.equal(true);
+			expect(board.checkDL(2, 1)).to.equal(true);
+			expect(board.checkDL(2, 2)).to.equal(true);
+			//switch to mixed board [a3] and X's turn
+			board.arr = a3;
+			board.turn = 1;
+			expect(board.checkDL(1, 1)).to.equal(false);
+			expect(board.checkDL(1, 2)).to.equal(false);
+			expect(board.checkDL(2, 2)).to.equal(true);
+		});
 	});
 });
