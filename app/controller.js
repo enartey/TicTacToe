694f6dfba2cfdff84d2c543fafe833d9989ee33b
Edwin Nartey <narteye15@hanover.edu>
2015-04-16 01:05:16 -0400

    fully linted

--- app/controller.js
+++ app/controller.js
@@ -1,31 +1,34 @@
-define(["jquery"],
+define([ "jquery" ],
 
 	function($){
-		var Controller;
-	});
+		// var Controller;
 
-	function Controller(domElement, model){
-		this.el = $(domElement);
-		this.model = model;
+		function Controller(domElement, model){
+			this.el = $(domElement);
+			this.model = model;
 
-		this.render();
-		this.model.on("change", this.render, this);
-		this.model.on("error", this.render, this);
+			this.render();
+			this.model.on("change", this.render, this);
+			this.model.on("error", this.render, this);
 
-		// If we end up using select and input in our work this will catch changes from the webpage 
-		//this.el.on("change", "input", this.respondToChange.bind(this));
-		//this.el.on("change", "select", this.respondToChange.bind(this));
-	}
-	
-	Controller.prototype = {
-		render: function(){
-			// set the HTML of element to equal result of applying template
-			this.el.html(template(this.model))
-		},
-		respondToChange: function(ev){
-			var el = $(ev.currentTarget);
-			this.model.set(el.data("key"), el.val());
+			// If we end up using select and input in our work this will catch changes from the webpage
+			// this.el.on("change", "input", this.respondToChange.bind(this));
+			// this.el.on("change", "select", this.respondToChange.bind(this));
 		}
-	};
-	return Controller;
-});
+
+		Controller.prototype = {
+			render: function(){
+				// set the HTML of element to equal result of applying model
+				this.el.html(this.model);
+			},
+			respondToChange: function(ev){
+				var el, i, j;
+				el = $(ev.currentTarget);
+				i = parseInt(el.id[ 0 ]);
+				j = parseInt(el.id[ 1 ]);
+				this.model.set(i, j);
+			}
+		};
+
+		return Controller;
+	});
